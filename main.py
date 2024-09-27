972533d4e00ff54d0965fc03c975e5cfc5bcbd4d
Epishko <timyrcivko2009@gmail.com>
2024-09-27 19:40:33 +0300

    finish

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import json
-with open('question.txt', 'r') as file:
+with open('question.txt', 'r', encoding='utf-8') as file:
     a = file.read()
 list_qwest = json.loads(a)
 score = 0
 list_ = []
-
+a = 10
 class Question():
     def __init__(self, text, deef, answer):
         self.text = text
@@ -12,20 +12,19 @@ class Question():
         self.answer = answer
         self.flag = False
         self.answ_player = None
-        self.point = self.get_points()
+        # self.point = self.get_points()
 
     def is_correct (self):
         self.answ_player = input('Ответ: ')
         if self.answ_player == self.answer:
-            print(self.answ_player)
             self.flag = True
-            self.build_positive_feedback()
+            print(self.build_positive_feedback())
         else:
-            print(5555)
             self.flag = False
-            self.build_negative_feedback()
-    def get_points(self):
-        return 10*self.deef
+            print(self.build_negative_feedback())
+
+    # def get_points(self):
+    #     return self.deef*10
 
     def build_question(self):
         return f"Вопрос: {self.text}\nСложность {self.deef}/5"
@@ -36,10 +35,25 @@ class Question():
     def build_negative_feedback(self):
         return f"Ответ неверный, верный ответ {self.answer}"
 
+    def __repr__(self):
+        return f'{self.text}'
+
 def create_qwest():
     for qweest in list_qwest:
         list_.append(Question(qweest['q'],qweest['d'], qweest['a']))
 
-create_qwest()
-print(list_[0].build_question())
-print(list_[0].is_correct())
+def get_report():
+    global end_score, score
+    create_qwest()
+    for quest in list_:
+        # report += f'{quest.build_question()}\n'
+        print(quest.build_question())
+        quest.is_correct()
+        if quest.flag:
+            score += int(quest.deef)
+            end_score = 10*score
+        else:
+            None
+    return end_score
+get_report()
+print(f'SCORE:{end_score}')
